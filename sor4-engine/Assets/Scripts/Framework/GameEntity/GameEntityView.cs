f4c889b1765d2730669f913f2b108b61dfe29985
Gil Costa <gsaurus@gmail.com>
2015-02-17 18:39:54 +0000

    face left/right

--- sor4-engine/Assets/Scripts/Framework/GameEntity/GameEntityView.cs
+++ sor4-engine/Assets/Scripts/Framework/GameEntity/GameEntityView.cs
@@ -4,6 +4,20 @@ using System.Collections;
 
 public class GameEntityView: View<GameEntityModel> {
 
-	// TODO: face in direction of the game entity (isFacedRight)
+
+	// Visual update
+	public override void Update(GameEntityModel model, float deltaTime){
+		
+		GameObject obj = UnityObjectsPool.Instance.GetGameObject(model.Index);
+		if (obj == null) return; // can't work without a game object
+
+		obj.transform.localScale = new Vector3(Mathf.Abs(obj.transform.localScale.x) * (model.isFacingRight ? 1 : -1), obj.transform.localScale.y, obj.transform.localScale.z);
+		
+	}
+	
+	public override bool IsCompatible(GameEntityModel originalModel, GameEntityModel newModel){
+		// No local data stored so it's always compatible with any PhysicPointModel
+		return true;
+	}
 
 }
