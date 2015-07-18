3759cb3ee8f8c58a36bf3715c5126167966faf6c
Gil Costa <gsaurus@gmail.com>
2015-07-18 09:51:19 +0100

    Initiating editor projects

---
+++ assets-packer/Assets/Scripts/Editor/ExportOnSave.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class ExportOnSave:UnityEditor.AssetModificationProcessor {
+
+	static string[] OnWillSaveAssets(string[] assets){
+		// Before saving, export asset bundles
+		Debug.Log("Go Save!");
+		//AssetDatabase.RemoveUnusedAssetBundleNames();
+		//BuildPipeline.BuildAssetBundles("AssetBundles");
+		return assets;
+	}
+
+}
---
+++ character-editor/Assets/Scripts/CharacterLoader.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterLoader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		string url = "file://" + Application.streamingAssetsPath + "/characters/rocha";
+		Debug.Log(url);
+		WWW www = WWW.LoadFromCacheOrDownload(url, 1);
+		if (www == null || www.assetBundle == null) return;
+		AssetBundle bundle = www.assetBundle;
+//		Debug.Log("going well.. " + bundle.name);
+//		foreach (string name in bundle.GetAllAssetNames()){
+//			Debug.Log(name);
+//		}
+		GameObject prefab = bundle.LoadAsset("prefab") as GameObject;
+//		if (prefab == null){
+//			Debug.Log("fail one");
+//			prefab = bundle.LoadAsset("assets/rocha/prefab.prefab") as GameObject;
+//			if (prefab == null){
+//				Debug.Log("Terrible!");
+//			}
+//		}
+//		Debug.Log("Prefab loaded!! " + prefab.name);
+		Animator animator = prefab.GetComponent<Animator>();
+//		if (animator != null){
+//			Debug.Log("animator!");
+//		}
+		RuntimeAnimatorController controller = animator.runtimeAnimatorController;
+		if (controller != null){
+			Debug.Log("Yay");
+			foreach(AnimationClip clip in controller.animationClips){
+				Debug.Log("clip " + clip.name);
+			}
+
+		}
+		//RuntimeAnimatorController animator = prefab.GetComponent<RuntimeAnimatorController>();
+
+	}
+	
+	// Update is called once per frame
+	void Update() {
+	
+	}
+}
