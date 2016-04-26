91cc03664213d4218554991335a00aaa109a63e0
Gil Costa <gil.costa@MPTadmins-MBP.home>
2016-04-26 17:53:11 +0100

    2D camera with hardcoded boundaries (will be updated when beat'em up camera is implemented)

--- unity-game/Assets/Scripts/SOR 2D/UnitySpecific/CameraTracker.cs
+++ unity-game/Assets/Scripts/SOR 2D/UnitySpecific/CameraTracker.cs
@@ -3,37 +3,31 @@ using System.Collections;
 
 public class CameraTracker : MonoBehaviour {
 
-	private float lerpValue = 0.01f;
+	private const float minCamX = -20;
+	private const float maxCamX = 20;
 
-	private Vector3 previousPos;
-	private Quaternion previousRot;
+	private const float lerpValue = 0.1f;
 
 	void Start(){
-		Camera mainCamera = Camera.main;
-		mainCamera.transform.position = transform.position + new Vector3(4,3,0);
-		mainCamera.transform.LookAt(transform.position);
-		previousPos = mainCamera.transform.position;
-		previousRot = mainCamera.transform.rotation;
+		
+	}
+
+	private float GetHalfCamWidth(Camera camera){
+		return camera.orthographicSize * camera.aspect;
 	}
 
 	// Update is called once per frame
 	void Update() {
 		Camera mainCamera = Camera.main;
 	
-		Vector3 target = new Vector3(Mathf.Lerp(0f, transform.position.x, 0.95f),
-		                             Mathf.Lerp(12f, transform.position.y + 2, 0.6f),
-		                             -12f
-		                             );
+		float halfCamWidth = GetHalfCamWidth(mainCamera);
+		Vector3 target = new Vector3(
+			Mathf.Clamp(transform.position.x, minCamX + halfCamWidth, maxCamX - halfCamWidth),
+			mainCamera.transform.position.y,
+			mainCamera.transform.position.z
+		);
 
 		mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, target, lerpValue);
-		mainCamera.transform.LookAt(transform.position);
-		if (lerpValue < 0.1f){
-			lerpValue = Mathf.Lerp(lerpValue, 0.1f, 0.01f);
-		}
-		mainCamera.transform.position = Vector3.Lerp(previousPos, mainCamera.transform.position, 0.2f);
-		mainCamera.transform.rotation = Quaternion.Lerp(previousRot, mainCamera.transform.rotation, 0.2f);
-		previousPos = mainCamera.transform.position;
-		previousRot = mainCamera.transform.rotation;
 	}
 
 }
