291fd80562ff2f6ca1e0d79a50e034b72fdf5f7f
Gil Costa <gil.costa@MPTadmins-MacBook-Pro.local>
2016-06-14 16:32:53 +0100

    cenas

---
+++ shooter-demo/shooter-model/Scripts/BulletModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace shootermodel
+{
+	public class BulletModel
+	{
+		public BulletModel()
+		{
+		}
+	}
+}
+
---
+++ shooter-demo/shooter-model/Scripts/ShipModel.cs
@@ -0,0 +1,67 @@
+using System;
+using RetroBread;
+using ProtoBuf;
+
+
+
+[ProtoContract]
+public class SkierModel{
+
+	[ProtoMember(1)]
+	public FixedFloat x;
+
+	[ProtoMember(2)]
+	public FixedFloat y;
+
+	[ProtoMember(3)]
+	public FixedFloat velX;
+
+	[ProtoMember(4)]
+	public FixedFloat velY;
+
+	// If you fall (by collision), take a little time to stand up
+	[ProtoMember(5)]
+	public uint fallenTimer;
+
+	// If you miss a checkpoint, be frozen for a few frames
+	[ProtoMember(6)]
+	public uint frozenTimer;
+
+	// Used to slowdown the skier when doing tight curves
+	[ProtoMember(7)]
+	public FixedFloat friction;
+
+	[ProtoMember(8)]
+	public FixedFloat targetVelX;
+	[ProtoMember(9)]
+	public FixedFloat targetVelY;
+	
+	[ProtoMember(10)]
+	public ModelReference inputModelRef;
+
+	// Default Constructor
+	public SkierModel(){
+		// set inactive
+		fallenTimer = 0;
+		frozenTimer = 0;
+	}
+
+	// Constructor
+	public SkierModel(FixedFloat x, FixedFloat y, ModelReference inputModelRef){
+		this.x = x;
+		this.y = y;
+		// moving straight down
+		this.velX = 0;
+		this.velY = 0;
+		this.targetVelX = 0;
+		this.targetVelY = -1.0f;
+
+		this.friction = 0.0f;
+
+		this.fallenTimer = 0;
+		this.frozenTimer = 0;
+		this.inputModelRef = inputModelRef;
+	}
+
+}
+
